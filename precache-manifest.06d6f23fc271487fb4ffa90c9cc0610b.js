4c7f7894230d50cf5e52868d162e7ddfbd8fcbdb
Gulara <ramazanova.gulare@gmail.com>
2022-10-16 01:18:56 +0400

    Updates

---
+++ precache-manifest.06d6f23fc271487fb4ffa90c9cc0610b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ad8f4953aaa59d56477",
+    "url": "/robofriends/static/css/main.4a59cd1b.chunk.css"
+  },
+  {
+    "revision": "9ad8f4953aaa59d56477",
+    "url": "/robofriends/static/js/main.9ad8f495.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "3b89fe4c3b204b97f118",
+    "url": "/robofriends/static/css/2.5a8092ae.chunk.css"
+  },
+  {
+    "revision": "3b89fe4c3b204b97f118",
+    "url": "/robofriends/static/js/2.3b89fe4c.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5f8cbc3215e8c800104a5b6bc9bfdad2",
+    "url": "/robofriends/index.html"
+  }
+];
